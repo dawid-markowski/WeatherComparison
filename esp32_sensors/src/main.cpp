367e25ccc97d5065b2ded75118fd92209f18cf4b
dawid-markowski <dawid.markowski.pl@gmail.com>
2025-02-08 19:34:41 +0100

    sending weather from esp to post endpoint

--- esp32_sensors/src/main.cpp
+++ esp32_sensors/src/main.cpp
@@ -1,12 +1,25 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 const int buttonPin = 0;
 bool IS_ON = 0;
 bool buttonPressed = false;
-long randTemp;
+long Temp;
+//String postAddr = "http://127.0.0.1:8000/post"; // web ip address
+const char* serverIP = "192.168.0.204"; // local ip addrss
+const int serverPort = 8000; // port for local communication
+String postAddr = String("http://") + serverIP + ":" + serverPort + "/post"; // complete url address
 
 const char* ssid = "GreenNet1";
 const char* password = "niunia1234";
+
+int TempGenerator(){
+  long randTemp;
+  randTemp = random(-10,10);
+  delay(5000);
+  return randTemp;
+}
 
 void setup(){
   Serial.begin(115200);
@@ -23,18 +36,18 @@ void setup(){
   Serial.println("Połączono z Wi-Fi");
   Serial.print("Adres IP: ");
   Serial.println(WiFi.localIP());
-
+  
   randomSeed(analogRead(0));
   pinMode(buttonPin,INPUT_PULLUP);
 }
 
 void loop(){
 int buttonState = digitalRead(buttonPin);
-Serial.println(buttonState);
+//Serial.println(buttonState);
 delay(300);
 
 
-//Jesli przycisk wcisniety
+//when button is pressed 1 is free 0 is pushed
 if(buttonState == 0 && !buttonPressed){
   IS_ON = !IS_ON;
   Serial.println("Zmiana stanu Pomiaru");
@@ -48,13 +61,33 @@ if(buttonState == 1){
 
 
 if(IS_ON){
-  TempGenerator();
+  Temp = TempGenerator();
+  JsonDocument doc;
+  doc["place"] = "Ozarow Mazowiecki";
+  doc["temp"] = Temp;
+  String payload;
+  serializeJson(doc, payload);
+  Serial.println("Wyslana wiadomosc:");
+  Serial.println(payload);
+
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.begin(postAddr.c_str());
+
+    int httpResponseCode = http.POST(payload);
+    
+    if(httpResponseCode > 0){
+      Serial.println("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else{
+      Serial.println("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+
+
+  }
 }
 
-}
-
-void TempGenerator(){
-  delay(5000);
-  randTemp = random(-10,10);
-  Serial.println(randTemp);
 }
